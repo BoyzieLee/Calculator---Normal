f81e37c8e693f8811104c58fae2237bded2e63ab
Boyzie Mathis <boyzielee@gmail.com>
2016-04-05 16:41:07 -0400

    defined variable except button

---
+++ js/main.js
@@ -0,0 +1,33 @@
+var input1 = document.querySelector('value-one');
+var input1 = document.querySelector('value-two');
+var button = document.querySelector('calculate-button');
+var answer = document.querySelector('answer');
+
+
+console.log('input1')
+console.log('input2')
+console.log('calculate-button')
+console.log('answer')
+
+
+// function calculate (input1 = input2) {
+// }
+var button = 'calculate-button';
+Button.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  var newText = myInput.value;
+
+  changeContent(newText);
+  console.log('answer')
+});
+
+var value = input1.value;
+// var answer = input1 + " " + input2;
+// var answer = input1 + input2;
+var answer = answer
+
+function answer (input1, input2){
+  answer += input1 + "(input2)";
+  console.log('answer')
+}
